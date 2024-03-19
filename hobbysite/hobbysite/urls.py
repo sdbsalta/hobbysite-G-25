d02985f927d3b5fb30787ef37475653096698a12
sdbsalta <95409312+sdbsalta@users.noreply.github.com>
2024-03-19 20:00:36 +0800

    Update urls.py
    added spaces to accommodate for merge

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -23,4 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('merchstore/', include('merchstore.urls', namespace = 'merchstore')),
+
+
+    
 ]
