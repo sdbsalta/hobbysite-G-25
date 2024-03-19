01a9bd3c684035965b208894d7f9678bf9017126
Veneza Vergara <venezavielle@gmail.com>
2024-03-19 21:02:21 +0800

    Remove Secret Key

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xx9)&h)+1yzk_^9_dtm_%wl4wo2jp8h2_%cma4g#-7^7l7!=o*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
