0728ce8db76028adfbc3e2bd0c6266a39e35121d
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-01 16:20:25 +0800

    added addtl details in product

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -16,7 +16,14 @@ class ProductType(models.Model):
         return self.name
 
 class Product(models.Model):
+    STATUS_CHOICES = [
+        ('Available'),
+        ('On sale'),
+        ('Out of stock')
+    ]
+
     name = models.CharField(max_length=255)
+    #TODO determined by developers and cannot be modified by regular users
     product_type = models.ForeignKey(
         ProductType, 
         on_delete=models.SET_NULL, 
@@ -24,6 +31,8 @@ class Product(models.Model):
         blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
     
     def __str__(self):
         return self.name
