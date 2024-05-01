d4caecab8087bcc2de574c8f8e6fba3fa6d513e7
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-01 16:28:13 +0800

    updated specs from Product List View

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -29,6 +29,10 @@ class Product(models.Model):
         on_delete=models.SET_NULL, 
         null=True, 
         blank=True)
+    owner = models.ForeignKey(
+        Profile, 
+        on_delete=models.CASCADE
+        )
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,12 +1,27 @@
 # merchstore/views.py
 
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Product
 
-class ProductTypeListView(ListView):
+class ProductTypeListView(LoginRequiredMixin, ListView):
     model = Product
     template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        all_products = Product.objects.exclude(user=self.request.user)
+        user_products = Product.objects.filter(user=self.request.user)
+
+        context['create_product_url'] = reverse_lazy('create_product')
+        context['all_products'] = all_products
+        context['user_products'] = user_products
+        
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
