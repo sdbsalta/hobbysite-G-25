a5179ed12d35d474c66053810d6b2dff92fe2152
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-01 15:31:10 +0800

    updated urls & viws to have profile (didn't add html yet in template_name)

--- hobbysite/user_manangement/views.py
+++ hobbysite/user_manangement/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Profile
+
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = ""
