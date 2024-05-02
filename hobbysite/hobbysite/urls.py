b4104256dc3a5910db9dc6ad5572157e3a67d005
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-02 17:15:55 +0800

    added profile to project url

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,11 +19,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+#TODO Remove? from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('merchstore/', include('merchstore.urls', namespace = 'merchstore')),
-
-
+    path('profile/', include('django.contrib.auth.urls')),
     
 ]
