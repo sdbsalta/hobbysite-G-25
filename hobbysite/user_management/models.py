596d491730e706f2f628f02cc912f7a311fbdbe6
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-02 17:50:02 +0800

    fixed content of profile (models, admin)

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,8 +1,13 @@
+#user_management/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+        )
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField()
     
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Profile
-
-class ProfileDetailView(LoginRequiredMixin, DetailView):
-    model = Profile
-    template_name = "profile_detail.html"
+# Create your views here.
