660bf03b8185e13708376816dcbd0a5c20243bc4
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-02 17:31:34 +0800

    created profiledetailview

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,7 +19,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-#TODO Remove? from django.conf import settings
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Profile
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = "profile_detail.html"
