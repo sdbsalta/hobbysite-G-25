90e3024b7e4d95ad12bffc011cc129fb541bd2c4
Jervie <jervie.manabat@student.ateneo.edu>
2024-05-03 15:38:04 +0800

    updated settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_management',
 
 
     'forum',
@@ -130,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/forum/threads"
+LOGOUT_REDIRECT_URL = "/profile/login"
