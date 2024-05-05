f91426f2c380f26deb1c58f3bbd55f5f4a197f7e
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-06 00:39:54 +0800

    added / at the end of redirect url and deleted migrations

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/home"
-LOGOUT_REDIRECT_URL = "/profile/login"
+LOGIN_REDIRECT_URL = "/home/"
+LOGOUT_REDIRECT_URL = "/profile/login/"
