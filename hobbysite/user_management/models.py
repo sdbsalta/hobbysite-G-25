caad748927f19c9b077d55702bf1fd4cb2ae58ca
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-05 11:56:23 +0800

    created models for user management

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+#user_management/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
+    display_name = models.CharField(max_length=63)
+    email_address = models.EmailField()
+    
+    def __str__(self):
+        return self.display_name
