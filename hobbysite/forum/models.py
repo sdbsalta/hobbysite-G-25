9d80bffbdef0d73d05e6e1d89b8077a9982b7360
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2024-05-06 23:11:27 +0800

    Updated models.py

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,26 +1,37 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from user_management.models import Profile
 
 
-class PostCategory(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Post Category Name")
-    description = models.TextField(verbose_name="Description")
-
+class ThreadCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 
     class Meta:
-        verbose_name_plural = "Post categories"
+        verbose_name_plural = "Thread categories"
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
-class Post(models.Model):
+class Thread(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    category = models.ForeignKey(
+        ThreadCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     entry = models.TextField()
+    image = models.ImageField(upload_to='thread_images', default='default.jpg')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
 
     class Meta:
         ordering = ['-created_on']
@@ -29,4 +40,28 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('forum:post_detail', args=[self.pk])
+        return reverse('forum:thread_detail', args=[self.pk])
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.thread.title}"
