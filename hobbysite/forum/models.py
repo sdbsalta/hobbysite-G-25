250e4087c0097f9b0014f93f404d0d406914b498
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2024-05-07 20:06:11 +0800

    remove some lines in a class in models.py

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -49,7 +49,7 @@ class Comment(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='forum_comments'
+        related_name='forum_comments',
     )
     thread = models.ForeignKey(
         Thread,
@@ -60,9 +60,6 @@ class Comment(models.Model):
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-created_on']
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.thread.title}"
