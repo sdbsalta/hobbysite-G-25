b28efba94ef16445c17602c0a3b897c14fe87152
Veneza Vergara <venezavielle@gmail.com>
2024-05-08 12:24:40 +0800

    FK = Profile

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,8 +1,11 @@
 # hobbysite/blog/models.py
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
+
+from user_management.models import Profile
+
+
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -16,9 +19,9 @@ class ArticleCategory(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     entry = models.TextField()
-    header_image = models.ImageField(upload_to='article_images', default='default.jpg') 
+    header_image = models.ImageField(upload_to='article_images', null=True) 
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -33,7 +36,7 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
