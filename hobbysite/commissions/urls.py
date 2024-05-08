ecd9d2542b7dcc56b52ba1c0fd020acc94541288
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-08 09:13:24 +0800

    fixed . into : for pk

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('list/', CommissionListView.as_view(), name='list'),
     path('detail/<int:pk>/', CommissionDetailView.as_view(), name='commission-detail'),
     path('add/', CommissionCreateView.as_view(), name='commission-add'),
-    path('<int.pk>/edit/', CommissionUpdateView.as_view(), name='commission-update'), 
+    path('<int:pk>/edit/', CommissionUpdateView.as_view(), name='commission-update'), 
 ]
 
 # This might be needed, depending on your Django version
