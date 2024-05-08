8a8be528c377b7377c638d2a950dc2e1b3c692ef
pochbarreiro <jose.barreiro@student.ateneo.edu>
2024-05-08 23:57:45 +0800

    Update branch to satisfy project specs

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -6,7 +6,14 @@ from .models import Commission, Job, JobApplication
 class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission
-        fields = ['title', 'description', 'status']
+        fields = [
+            'title',
+            'description',
+            'status',
+            'created_on',
+            'updated_on',
+            'author'
+            ] 
         widgets = {
             'status': forms.Select(choices=Commission.STATUS_CHOICES),
         }
--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -7,37 +7,90 @@ from django.utils import timezone
 from user_management.models import Profile
 
 from .models import Commission, Job, JobApplication
-# import from forms if needed later
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Commission, Job, JobApplication
+from .forms import CommissionForm, JobForm, JobApplicationForm
 
 class CommissionListView(ListView):
     model = Commission
-    template_name = 'commissions_list.html'
+    template_name = 'commissions/commissions_list.html'
     ordering = ['-created_on']
 
-class CommissionDetailView(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['user_commissions'] = Commission.objects.filter(author=self.request.user).order_by('-created_on')
+            context['user_applications'] = JobApplication.objects.filter(applicant=self.request.user.profile).order_by('-applied_on')
+        return context
+
+class CommissionDetailView(LoginRequiredMixin, DetailView):
     model = Commission
-    template_name = 'commissions_detail.html'
+    template_name = 'commissions/commissions_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        jobs = Job.objects.filter(commission=self.object)
+
+        for job in jobs:
+            job.is_full = job.job_application_set.filter(status='Accepted').count() >= job.manpower_required
+
+        context['jobs'] = jobs
+        context['total_manpower_required'] = sum(job.manpower_required for job in jobs)
+        context['total_open_manpower'] = sum(job.manpower_required - job.job_application_set.filter(status='Accepted').count() for job in jobs)
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        job_id = request.POST.get('job_id')
+        job = get_object_or_404(Job, id=job_id)
+        JobApplication.objects.create(job=job, applicant=request.user.profile)
+        return redirect('commissions:commission_detail', pk=self.object.pk)
 
 class CommissionCreateView(LoginRequiredMixin, CreateView):
     model = Commission
-    template_name = 'commissions_form.html'
+    template_name = 'commissions/commissions_form.html'
     fields = [
         'title',
         'description',
-        'status']
+        'status',
+        'created_on',
+        'updated_on'
+        ]
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = context['form']
+        form.fields['author'].initial = Profile.objects.get(user=self.request.user)
+        form.fields['author'].disabled = True
+        form.fields['created_on'].disabled = True
+        form.fields['updated_on'].disabled = True
 
 class CommissionUpdateView(LoginRequiredMixin, UpdateView):
     model = Commission
-    template_name = 'commissions_form.html'
-    fields = ['title', 'description', 'status']
+    template_name = 'commissions/commissions_form.html'
+    fields = [
+        'title',
+        'description',
+        'status',
+        'created_on',
+        'updated_on'
+        ]
+    login_url = 'login'
 
     def form_valid(self, form):
-        form.instance.updated_on = timezone.now()
+        commission = form.save(commit=False)
+        if all(job.status == 'Full' for job in commission.job.all()):
+            commission.status = 'Full'
+        commission.updated_on = timezone.now()
+        commission.save()
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = context['form']
+        form.fields['author'].initial = Profile.objects.get(user=self.request.user)
+        form.fields['author'].disabled = True
+        form.fields['created_on'].disabled = True
