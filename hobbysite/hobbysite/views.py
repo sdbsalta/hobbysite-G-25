f1b713fec383952deb6c618af5955f6136cbb58f
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-08 22:27:12 +0800

    made a index html n view

--- hobbysite/hobbysite/views.py
+++ hobbysite/hobbysite/views.py
@@ -1,5 +1,10 @@
 #hobbysite/views.py
 from django.shortcuts import render
+from django.views import View
 
 def home(request):
     return render(request, 'home.html')
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
