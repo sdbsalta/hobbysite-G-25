acb4b8ea447cc210965bc3bbe48de91805af073b
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-05-08 22:32:05 +0800

    removed indexs, didnt wokr

--- hobbysite/hobbysite/views.py
+++ hobbysite/hobbysite/views.py
@@ -4,7 +4,3 @@ from django.views import View
 
 def home(request):
     return render(request, 'home.html')
-
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
