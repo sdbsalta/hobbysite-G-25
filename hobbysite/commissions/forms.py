ed322c6cbf02645e167203355d0719e9786ae063
pochbarreiro <jose.barreiro@student.ateneo.edu>
2024-05-08 08:30:00 +0800

    Update forms as specified

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -3,3 +3,23 @@ from django.contrib import admin
 
 from .models import Commission, Job, JobApplication
 
+class CommissionForm(forms.ModelForm):
+    class Meta:
+        model = Commission
+        fields = ['title', 'description', 'status']
+        widgets = {
+            'status': forms.Select(choices=Commission.STATUS_CHOICES),
+        }
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['role', 'manpower_required', 'status']
+        widgets = {
+            'status': forms.Select(choices=Job.STATUS_CHOICES),
+        }
+
+class JobApplicationForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = []
